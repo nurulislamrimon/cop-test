90fe8462f418dbe17919332319f1a88344dd4bd1
Nurul Islam Rimon <nurulislamrimon@gmail.com>
2022-06-04 00:23:41 +0600

    JWT done

--- src/Pages/Finance/Finance.js
+++ src/Pages/Finance/Finance.js
@@ -2,28 +2,34 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
+import { useNavigate } from 'react-router-dom'
 
 const Finance = () => {
     const [users, setUsers] = useState([]);
     const [logedUser] = useAuthState(auth);
     const email = logedUser?.email;
+    const navigate = useNavigate();
 
     useEffect(() => {
-        const getFinance = () => {
-            fetch(`http://localhost:5000/finance?email=${email}`, {
-                headers: {
-                    authorization: `${localStorage.getItem('accessToken')}`
-                }
+        const getFinance = async () => {
+            const { data } = await axios.get(`http://localhost:5000/finance?email=${email}`, {
+                headers: { authorization: `${localStorage.getItem('accessToken')}` }
             })
-                .then(res => res.json())
-                .then(data => setUsers(data))
+            setUsers(data)
+            // fetch(`http://localhost:5000/finance?email=${email}`, {
+            //     headers: {
+            //         authorization: `${localStorage.getItem('accessToken')}`
+            //     }
+            // })
+            //     .then(res => res.json())
+            //     .then(data => setUsers(data))
         }
         getFinance()
     }, [email])
 
     return (
         <div>
-            {users.map(user => <h3 key={user._id}>{user.name}</h3>)}
+            {Array.isArray(users) ? users.map(user => <h3 key={user?._id}>{user?.name}</h3>) : navigate('/login')}
         </div>
     );
 };
